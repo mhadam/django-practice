48392a2c119d94abbd3dcddf922fad9379de570e
mhadam <mikehadam@gmail.com>
2016-05-30 13:46:04 -0400

    added about template, and some static media files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,5 +7,5 @@ def index(request):
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page! <br/> <a href='/rango/'>Index</a>")
-
+	context_dict = {'bpb' : "I see a darkness"}
+	return render(request, 'rango/about.html', context_dict)
