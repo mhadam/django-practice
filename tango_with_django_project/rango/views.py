1b947bcabc10213c78410ee3fdaa7d1e8dfe86ec
mhadam <mikehadam@gmail.com>
2016-05-30 10:59:36 -0400

    added basic templating

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("Rango says hey there world! <br/> <a href='/rango/about'>About</a>")
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	return HttpResponse("Rango says here is the about page! <br/> <a href='/rango/'>Index</a>")
+
