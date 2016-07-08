9c5a6cb7a00a1dee50c596f8d48a1afe809afaf3
mhadam <mikehadam@gmail.com>
2016-07-07 23:36:57 -0400

    added AJAX like button to category page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -11,6 +11,22 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from rango.bing_search import run_query
 
+@login_required
+def like_category(request):
+
+    cat_id = None
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+
+    likes = 0
+    if cat_id:
+        cat = Category.objects.get(id=int(cat_id))
+        if cat:
+            likes = cat.likes + 1
+            cat.likes = likes
+            cat.save()
+    return HttpResponse(likes)
+
 def track_url(request):
     page_id = None
     url = '/rango/'
